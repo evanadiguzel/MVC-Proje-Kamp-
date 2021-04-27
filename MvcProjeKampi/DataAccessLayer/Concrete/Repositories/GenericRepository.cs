7cc82162c1dbdbfb0b10ef5353deeee4730c0ead
evanadiguzel <evanadiguzel@gmail.com>
2021-04-28 00:25:20 +0300

    MVC Proje Kampi
    MVC Proje Kampi odev

---
+++ MvcProjeKampi/DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+        DbSet<T> _object;
+        public GenericRepository()
+        {
+            _object = c.Set<T>(); // Contexte bağlı olarak gönderilen T değeri.Dışarıdan Gönderilen T değeri _object üzerine atandı.
+
+        }
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List()
+        {
+           return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList(); //filterda ne verdiysek onu listeleyerek dönecek
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() //Verilerin listelendiği kısım
+        {
+            return View();
+        }
+
+        public ActionResult About() 
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Test() // Türü ActionResult, geriye dönüş değeri döndürüyor
+        {
+            return View(); // Bana geriye bir sayfa döndür
+        }
+    }
+}
